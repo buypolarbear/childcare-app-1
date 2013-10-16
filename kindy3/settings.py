2f65172fa4cdca3048cc177d7ba6eaf63ca511ed
Matej Ramuta <matej.ramuta@gmail.com>
2013-10-16 13:20:06 +0200

    Heroku upload5

--- kindy3/settings.py
+++ kindy3/settings.py
@@ -82,19 +82,23 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(DIRNAME, 'static/')
+# TODO: Heroku
+#STATIC_ROOT = os.path.join(DIRNAME, 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+# TODO: Heroku
+#STATIC_URL = '/static/'
 
 # Additional locations of static files
+# TODO: Heroku
+'''
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
-
+'''
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
@@ -183,9 +187,6 @@ LOGGING = {
     }
 }
 
-ANONYMOUS_USER_ID = -1
-GUARDIAN_RENDER_403 = True
-
 # TODO: UPLOAD NA HEROKU!!!
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
@@ -212,3 +213,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend', # this is default
     'guardian.backends.ObjectPermissionBackend',
 )
+
+ANONYMOUS_USER_ID = -1
+GUARDIAN_RENDER_403 = True
