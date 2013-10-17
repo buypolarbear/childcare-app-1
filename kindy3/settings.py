8540c29cf4714c9b0af72e26a80dc3a46cb6d682
Davor Padovan <davor.padovan@gmail.com>
2013-10-17 13:33:33 +0200

    Basic dashboard design implemented.

--- kindy3/settings.py
+++ kindy3/settings.py
@@ -76,7 +76,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(DIRNAME, 'static/')
+STATIC_ROOT = os.path.join(DIRNAME, 'static-serve/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -87,6 +87,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(DIRNAME, 'static').replace('\\', '/'),
 )
 
 # List of finder classes that know how to find static files in
