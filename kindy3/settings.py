280d4c88ffb68eb075bea68ef257c902f43bb5b2
Matej Ramuta <matej.ramuta@gmail.com>
2013-10-20 16:51:34 +0200

    fixing a static files problem5

--- kindy3/settings.py
+++ kindy3/settings.py
@@ -226,7 +226,7 @@ if not LOCAL_ENV_BOOL:
     #import os
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     #STATIC_ROOT = 'staticfiles'
-    STATIC_ROOT = 'static'
+    STATIC_ROOT = 'staticfiles'
     STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
